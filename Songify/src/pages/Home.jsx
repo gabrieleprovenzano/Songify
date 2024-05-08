851203a6dc4abd0ca9970c405b139a9d6e4f2021
Gabriele <gabriele.provenzano00@gmail.com>
2024-05-08 15:19:33 +0200

    push

---
+++ Songify/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/Navbar"
+import "./styles/home.css"
+
+export function Home() {
+    return(
+        <div className="home-container">
+            <Navbar />
+        </div>
+    )
+}
